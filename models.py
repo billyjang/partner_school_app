fda1afcb1efa8fa8e0df351059eecc39a9f1dcf8
William Jang <wjang20@amherst.edu>
2020-01-30 16:50:17 -0500

    model change

--- models.py
+++ models.py
@@ -12,6 +12,7 @@ class User(db.Model):
     email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
 
+    entries = db.relationship('Entry', backref='User', lazy=True)
     
     def __init__(self, userid=None, firstName=None, lastName=None, childFirstName=None, childLastName=None, userRole=None, email=None, password=None):
         self.userid=userid
@@ -27,6 +28,23 @@ class User(db.Model):
         # TODO: might want to get rid of password in repr
         return "<User(userid={}, firstName={}, lastName={}, childFirstName={}, childLastName={}, userRole={}, email={}, password={})>".format(self.userid, self.firstName, self.lastName,
                 self.childFirstName, self.childLastName, self.userRole, self.email, self.password)
+
+class Entry(db.Model):
+    entryid = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.userid'))
+    target_behavior = db.Column(db.String(255), nullable=False)
+    home_school_goal = db.Column(db.String(255), nullable=False)
+    action_plan_one = db.Column(db.String(255), nullable=False)
+    action_plan_two = db.Column(db.String(255))
+    action_plan_three = db.Column(db.String(255))
+    action_plan_four = db.Column(db.String(255))
+    action_plan_five = db.Column(db.String(255))
+    action_plan_one_rating = db.Column(db.Integer(), nullable=False)
+    action_plan_two_rating = db.Column(db.Integer())
+    action_plan_three_rating = db.Column(db.Integer())
+    action_plan_four_rating = db.Column(db.Integer())
+    action_plan_five_rating = db.Column(db.Integer())
+    date = db.Column(db.Date())
 '''
 class Entry(db.Model):
     __tablename__ = 'entry'
