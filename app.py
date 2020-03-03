1b8c76e3631a9c2a766b722a6ef77efccdc018b2
William Jang <wjang20@amherst.edu>
2020-03-03 03:07:38 -0500

    admin priv and dynamic entry

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
-from flask import render_template, url_for, request, redirect, session, flash
+from flask import render_template, url_for, request, redirect, session, flash, jsonify
 #from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from crypt import pwd_context
 import json
+import os
 
 app = Flask(__name__)
 #sess = Session()
 
 app.debug = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kjmuultbvkoaqd:56ac542cebad0a7473deb0c24d9a87daab81dd0511b4925f9e552fe834d1f1fd@ec2-34-193-42-173.compute-1.amazonaws.com:5432/d76s1h363rpp0'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kjmuultbvkoaqd:56ac542cebad0a7473deb0c24d9a87daab81dd0511b4925f9e552fe834d1f1fd@ec2-34-193-42-173.compute-1.amazonaws.com:5432/d76s1h363rpp0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ryehoqoimamdpg:7420762e6c90e7ffadefb487e90e9844769d64b7b03035d8c86ec004954dda05@ec2-34-199-149-157.compute-1.amazonaws.com:5432/d11d2f8nejvhgg'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = 'super secret key'
@@ -25,22 +27,25 @@ loggedIn=False
 # TODO: make sure the get and post is clear so authentication actually means something
 # TODO: screenreader accessibility
 # TODO: changing the exception handling
+# TODO: I don't think messages are happening
+# TODO: Fix xss vulnerabilities
+# TODO: PROPER COMPLAINING
 
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.method=='POST':
-        session.pop('email', None)
-        user_email = request.form['email']
-        matching_emails = User.query.filter_by(email=user_email)
-        if matching_emails.count() != 1:
+        session.pop('userID', None)
+        userID = request.form['userID']
+        matching_ids = User.query.filter_by(idNum=userID)
+        if matching_ids.count() != 1:
             return redirect(url_for('home'))
         else:
-            session['email'] = user_email
+            session['userID'] = userID
             password = request.form['password']
-            verify_password = matching_emails.with_entities(User.password).all()[0][0]
+            verify_password = matching_ids.with_entities(User.password).all()[0][0]
             if pwd_context.verify(password, verify_password):
-                print(matching_emails.with_entities(User.userRole).all()[0][0])
-                session['role'] = matching_emails.with_entities(User.userRole).all()[0][0]
+                print(matching_ids.with_entities(User.userRole).all()[0][0])
+                session['role'] = matching_ids.with_entities(User.userRole).all()[0][0]
                 return redirect(url_for('addentry'))
             else:
                 return redirect(url_for('home'))            
@@ -66,17 +71,15 @@ def newaccountsuccess():
             flash('Passwords must match')
             return redirect(url_for('newaccount'))
         email=request.form['email']
-        firstName = request.form['firstName']
-        lastName = request.form['lastName']
-        cfirstName = request.form['childFirstName']
-        clastName = request.form['childLastName']
+        userID = request.form['userID']
         userRole = request.form['userRole']
-        email = request.form['email']
+        targetBehavior=request.form['targetBehavior']
+        homeSchoolGoal=request.form['homeSchoolGoal']
         # TODO: check if passwords are the same in javascript or jquery
         password = request.form['password']
         hashed = pwd_context.hash(password)
         try:
-            new_user = User(firstName, lastName, cfirstName, clastName, userRole, email, hashed)
+            new_user = User(userID,userRole, hashed, targetBehavior, homeSchoolGoal,email)
             db.session.add(new_user)
             db.session.commit()
             return render_template('newaccountsuccess.html')
@@ -91,40 +94,54 @@ def newaccountsuccess():
 def landing():
     return render_template('landing.html')
 
+@app.route('/notecalendar')
+def notecalendar():
+    return render_template('notecalendar.html')
+
+# TODO: There is an ordering that must be followed--does something break if Gazi doesn't enter in the values in time first?
+
 # adding entry and viewing data
 @app.route('/addentry') # add required message here?
 def addentry():
     try:
-        if session['email'] != None:
-            print(session.get('email'))
+        if session['userID'] != None:
+            print(session.get('userID'))
             print(session.get('role'))
-            return render_template('addentry.html', data=session.get('role', None))
+            current_user = User.query.filter_by(idNum=session['userID']).all()[0]
+            #return render_template('addentry.html', data=session.get('role', None)
+            user_data = {'userID' : current_user.idNum, 'role' : current_user.userRole, 'targetBehavior' : current_user.targetBehavior,
+                        'homeSchoolGoal' : current_user.homeSchoolGoal}
+            return render_template('addentry.html', data=user_data)
     except:
         return "To fill in later. Redirect to login."
 
 @app.route('/submitted', methods=['POST'])
 def submitted():
     if request.method=='POST':
-        firstName=request.form['firstName']
-        lastName=request.form['lastName']
+        userID=request.form['userID']
+        date=request.form['date']
         targetBehavior=request.form['targetBehavior']
         homeSchoolGoal=request.form['homeSchoolGoal']
         actionPlans=request.form.getlist('actionPlan')
-        date=request.form['date']
         goalRange=None
         if session['role'] == 'Teacher':
             goalRange = request.form['goalRangeTeacher']
         else:
             goalRange = request.form['goalRangeParent']
-        if firstName=='' or lastName=='' or targetBehavior=='' or homeSchoolGoal=='' or actionPlans==None:
+        print(userID)
+        print(goalRange)
+        print(targetBehavior)
+        print(homeSchoolGoal)
+        print(actionPlans)
+        if userID=='' or goalRange==None or targetBehavior=='' or homeSchoolGoal=='' or actionPlans==None:
             #return render_template('addentry.html', message="*Please fill out required fields*")
             flash("Must fill out all fields")
             return redirect(url_for('addentry'))
         else:
             print(date)
-            current_user = User.query.filter_by(email=session['email']).all()[0]
+            current_user = User.query.filter_by(idNum=session['userID']).all()[0]
             mapping = {"Situation significantly worse":-2, "Situation somewhat worse":-1, "No progress":0, "Situation somewhat better": 1, "Situation significantly better":2}
-            new_entry = Entry(user_id=current_user.userid, target_behavior=targetBehavior, home_school_goal=homeSchoolGoal, goal_rating=mapping[goalRange], date=date)
+            new_entry = Entry(user_id=current_user.idNum, goal_rating=mapping[goalRange], date=date)
             # TODO: more elegant way to do this. FOR TESTING PURPOSES ONLY
             new_entry.action_plan_one = actionPlans[0]
             if(len(actionPlans) > 1):
@@ -144,8 +161,8 @@ def submitted():
 @app.route('/readdata')
 def readdata():
     try:
-        if session['email'] != None:
-            current_user = User.query.filter_by(email=session['email']).all()[0]
+        if session['userID'] != None:
+            current_user = User.query.filter_by(idNum=session['userID']).all()[0]
             all_entries = current_user.entries
             table_data = json.dumps([entry.serialize() for entry in all_entries])
             return render_template('readdata.html', data=table_data)
@@ -155,9 +172,30 @@ def readdata():
 @app.route('/logout')
 def logout():
     try:
-        if session['email'] != None:
-            session.pop('email', None)
+        if session['userID'] != None:
+            session.pop('userID', None)
             session.pop('role', None)
             return redirect(url_for('home'))
     except KeyError:
         return redirect(url_for('home'))
+
+@app.route('/adminlanding')
+def adminlanding():
+    return render_template('adminlanding.html')
+
+# TODO: finish these after config the db again
+@app.route('/adminentry')
+def adminentry():
+    all_users = User.query.all()
+    all_data = []
+    for user in all_users:
+        user_entry = {'userID' : user.idNum, 'actionPlans' : user.actionplans}
+        all_data.append(user_entry)
+    return render_template('adminentry.html', data=all_data)
+
+@app.route('/adminpost', methods=["POST"])
+def adminpost():
+    #actionPlan = request.form['actionPlan1']
+    #print(actionPlan)
+    #return redirect(url_for('adminentry'))
+    return "success"
--- models.py
+++ models.py
@@ -3,36 +3,51 @@ from manage import db, app
 class User(db.Model):
     __tablename__ = 'User'
     #uid = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    userid = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-    firstName = db.Column(db.String(80), nullable=False)
-    lastName = db.Column(db.String(80), nullable=False)
-    childFirstName = db.Column(db.String(80), nullable=False)
-    childLastName = db.Column(db.String(80), nullable=False)
+    idNum = db.Column(db.Integer, primary_key=True, autoincrement=False, unique=True)
     userRole = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
+    targetBehavior = db.Column(db.String(255), nullable=False)
+    homeSchoolGoal = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
 
     entries = db.relationship('Entry', backref='User', lazy=True)
+    actionplans = db.relationship('ActionPlan', backref='User', lazy=True)
 
-    def __init__(self, firstName=None, lastName=None, childFirstName=None, childLastName=None, userRole=None, email=None, password=None):
-        self.firstName=firstName
-        self.lastName=lastName
-        self.childFirstName=childFirstName
-        self.childLastName=childLastName
+    def __init__(self, idNum=None, userRole=None, password=None, targetBehavior=None, homeSchoolGoal=None, email=None):
+        # TODO: if idNum is None, throw an exception so it can be handled with appropriately.
+        self.idNum=idNum
         self.userRole=userRole
-        self.email=email
         self.password=password
+        self.targetBehavior=targetBehavior
+        self.homeSchoolGoal=homeSchoolGoal
+        self.email=email
     
+    def serialize(self):
+        return {
+            'userID' : self.idNum,
+            'role' : self.userRole,
+            'targetBehavior' : self.targetBehavior,
+            'homeSchoolGoal' : self.homeSchoolGoal,
+            'email' : self.email
+        }
+
     def __repr__(self):
         # TODO: might want to get rid of password in repr
-        return "<User(userid={}, firstName={}, lastName={}, childFirstName={}, childLastName={}, userRole={}, email={}, password={})>".format(self.userid, self.firstName, self.lastName,
-                self.childFirstName, self.childLastName, self.userRole, self.email, self.password)
+        #return "<User(userid={}, firstName={}, lastName={}, childFirstName={}, childLastName={}, userRole={}, email={}, password={})>".format(self.userid, self.firstName, self.lastName,
+        #        self.childFirstName, self.childLastName, self.userRole, self.email, self.password)
+        return "<User(idNum={}, userRole={})>".format(self.idNum, self.userRole)
+
+class ActionPlan(db.Model):
+    __tablename__ = 'ActionPlan'
+    actionPlanId = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
+    userId = db.Column(db.Integer, db.ForeignKey('User.idNum'))
+    stepName = db.Column(db.String(255))
+    order = db.Column(db.Integer)
 
 class Entry(db.Model):
+    __tablename__ = 'Entry'
     entryid = db.Column(db.Integer, primary_key=True, autoincrement=True, unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.userid'))
-    target_behavior = db.Column(db.String(255), nullable=False)
-    home_school_goal = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.idNum'))
     action_plan_one = db.Column(db.String(255), nullable=False)
     action_plan_two = db.Column(db.String(255))
     action_plan_three = db.Column(db.String(255))
@@ -53,8 +68,6 @@ class Entry(db.Model):
             action_plans.append(self.action_plan_five)
         return {
             'date':self.date.isoformat(),
-            'target_behavior':self.target_behavior,
-            'home_school_goal':self.home_school_goal,
             'action_plans':action_plans,
             'goal_rating':self.goal_rating,
             'user_id':self.user_id
